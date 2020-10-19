1c991a405b7899c6977160033a9ee4282666f2cd
Daniel Baynton <Daniel.Baynton@hotmail.com>
2020-10-19 12:18:34 +0100

    Create main window

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -24,5 +24,48 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void TitleBarButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Label button = sender as Label;
+            button.Background = new SolidColorBrush(new Color()
+            {
+                R = 63,
+                G = 63,
+                B = 65,
+                A = 100
+            });
+        }
+
+        private void TitleBarButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Label button = sender as Label;
+            button.Background = new SolidColorBrush(new Color()
+            {
+                A = 0
+            });
+        }
+
+        private void TitleBarButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Label button = sender as Label;
+            if (button.Name == "CloseButton")
+            {
+                Close();
+            }
+            else if (button.Name == "MinimiseButton")
+            {
+                WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                WindowState = WindowState.Normal;
+                DragMove();
+            }
+        }
     }
 }
